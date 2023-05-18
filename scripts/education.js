3f0be3497d511a734d67bec2c8046c213687b282
Matias Bacci <matiasbacci@outlook.es>
2023-05-18 02:20:16 -0300

    Primer carga de proyecto

---
+++ scripts/education.js
@@ -0,0 +1,34 @@
+const degrees = [
+    {
+        establishment: "UTN - Mar del Plata",
+        career: "University degree in Programming",
+        period: "February, 2022 - December 2025 (estimated)", 
+    },
+    {
+        establishment: "Juan Gutenberg Institute",
+        career: "High School diploma in natural sciences",
+        period: "Finished in 2006",
+    },
+
+    ]
+
+
+const education = document.getElementById("education");
+
+for (let course of degrees) {
+    education.innerHTML += `
+    <div class="education">
+        <h1>${course.establishment}</h1>
+        <p><b>Career</b>: ${course.career}</p>
+        <p><b>Period</b>: ${course.period}</p>
+    </div>`
+}
+
+for (let course of degrees) {
+    education.innerHTML += `
+    <div class="education">
+        <h1>${course.establishment}</h1>
+        <p><b>Career</b>: ${course.career}</p>
+        <p><b>Period</b>: ${course.period}</p>
+    </div>`
+}
